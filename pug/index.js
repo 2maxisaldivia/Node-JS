4fb8c81df0bd037d6bac5be73b8bc3b692663d9a
2maxisaldivia <saldiviamaximiliano2@gmail.com>
2022-09-23 15:23:12 -0300

    finish pug

--- pug/index.js
+++ pug/index.js
@@ -1,22 +1,42 @@
-const express = require('express')
-const apiRoutes = require("./routers/app.routers")
-const path = require("path")
-const PORT = process.env.PORT || 8080
+const express = require("express");
+const path = require("path");
+const Products = require("./model/data");
+
+const app = express();
+const products = new Products();
+const { items } = products;
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 
-const app = express()
+app.set("views", "./views")
+app.set("view engine", "pug")
 
-// Parsea el body de una peticion 
-app.use(express.json())
-app.use(express.static('public'))
-//app.use(express.urlencoded({extended: true}))
+app.post("/productos", (req, res) => {
+  const data = req.body;
+  const { title, price, thumbnail } = req.body;
+  if (title || price || thumbnail) {
+    products.save(data);
+    res.redirect("/productos");
+  }
+});
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "./nav-app/index.html"))
-// })
-
-app.use("/api", apiRoutes)
-
+app.get("/productos", (req, res) => {
+  if (items.length >= 1) {
+    res.render("index", {
+      mostrarProductos: true,
+      products: items,
+    });
+  } else {
+    res.render("index", {
+      mostrarProductos: false,
+    });
+  }
+});
 
 const connectedServer = app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`)
@@ -25,4 +45,3 @@ const connectedServer = app.listen(PORT, () => {
 connectedServer.on('error', error => {
     console.log(error.message)
 })
-
