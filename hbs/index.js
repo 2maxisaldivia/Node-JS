3836b9ae091f3bce2a050d6871334c7ddf9f98e8
Maximiliano Saldivia <saldiviamaximiliano2@gmail.com>
2022-09-23 11:38:58 -0300

    finish hbs

--- hbs/index.js
+++ hbs/index.js
@@ -1,22 +1,49 @@
-const express = require('express')
-const apiRoutes = require("./routers/app.routers")
-const path = require("path")
-const PORT = process.env.PORT || 8080
+const express = require("express");
+const { engine } = require("express-handlebars");
+const path = require("path");
+const Products = require("./model/data");
 
+const app = express();
+const products = new Products();
+const { items } = products;
 
-const app = express()
+const PORT = process.env.PORT || 8080;
 
-// Parsea el body de una peticion 
-app.use(express.json())
-app.use(express.static('public'))
-//app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "./nav-app/index.html"))
-// })
+app.engine("hbs", engine({
+    extname: "hbs",
+    defaultLayout: "main.hbs",
+    layoutsDir: path.resolve(__dirname, "./views/layouts"),
+    partialsDir: path.resolve(__dirname, "./views/partials")
+}))
 
-app.use("/api", apiRoutes)
+app.set("views", "./views")
+app.set("view engine", "hbs")
 
+app.post("/productos", (req, res) => {
+  const data = req.body;
+  const { title, price, thumbnail } = req.body;
+  if (title || price || thumbnail) {
+    products.save(data);
+    res.redirect("/productos");
+  }
+});
+
+app.get("/productos", (req, res) => {
+  if (items.length >= 1) {
+    res.render("index", {
+      mostrarProductos: true,
+      products: items,
+    });
+  } else {
+    res.render("index", {
+      mostrarProductos: false,
+    });
+  }
+});
 
 const connectedServer = app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`)
