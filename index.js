38b6d855eddd03b99fa7f39078dc038bff936cfe
2maxisaldivia <saldiviamaximiliano2@gmail.com>
2022-11-14 16:58:20 -0300

    back to desafio-1

--- index.js
+++ index.js
@@ -2,6 +2,25 @@ const express = require('express');
 const authorizer = require('./middlewares/authorizer');
 const apiRoutes = require("./routers/app.routers")
 const PORT = process.env.PORT || 8080
+const admin = require("firebase-admin")
+const { getFirestore } = require('firebase-admin/firestore')
+
+var serviceAccount = require("./data/firebase/firebase.config.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+console.log("firebase")
+
+const makeRequest = async () => {
+  try {
+    const db = getFirestore()
+    const query = db.collection("")
+  } catch {
+
+  }
+}
 
 const app = express()
 
